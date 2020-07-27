591265a5f148f33540f8a1f8ccefdf2fea149254
SadhanaRajan <sadhu712@gmail.com>
2020-07-27 10:40:20 -0700

    Update checkForSum.js

--- checkForSum.js
+++ checkForSum.js
@@ -1,4 +1,7 @@
-/*Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+/*
+#1 from Daily Coding Challenge - on - July 26th 2020
+
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
